6064e1dcb115f6484daecf2a153d3b6ce2dbc981
Phillip-mulindwa <p.mulindwa@alustudent.com>
2026-02-12 15:35:57 +0000

    tables

---
+++ library_management/lib/doctype/membership_plan/test_membership_plan.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Phillip Mulindwa and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestMembershipPlan(FrappeTestCase):
+	pass

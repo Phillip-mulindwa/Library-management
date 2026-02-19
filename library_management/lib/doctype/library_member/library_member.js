c96b496960c966a8564054d436596585c4b5c44d
Phillip-mulindwa <p.mulindwa@alustudent.com>
2026-02-19 14:57:04 +0000

    error

--- library_management/lib/doctype/library_member/library_member.js
+++ library_management/lib/doctype/library_member/library_member.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2026, Phillip Mulindwa and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Library Member", {
-// 	refresh(frm) {
+ frappe.ui.form.on("Library Member", {
+ 	refresh(frm) {
 
-// 	},
-// });
+
+ 	},
+
+      first_name(frm){
+         setfullname(frm)
+      },
+      last_name(frm){
+        setfullname(frm)
+      },
+      after_save(frm){
+        frappe.msgprint(`Hallo ${frm.doc.full_name}`)
+
+      }
+
+ });
+      function setfullname(frm){
+        first_name = frm.doc.first_name || ""
+        last_name = frm.doc.last_name || ""
+        f_name = `${first_name} ${last_name}`
+        frm.set_value("full_name",f_name)
+      }
--- library_management/lib/doctype/loan/loan.js
+++ library_management/lib/doctype/loan/loan.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2026, Phillip Mulindwa and contributors
 // For license information, please see license.txt
+frappe.ui.form.on("Loan", {
 
-// frappe.ui.form.on("Loan", {
-// 	refresh(frm) {
+    validate(frm) {
 
-// 	},
-// });
+        if (frm.doc.return_date && frm.doc.transaction_date) {
+
+            if (frm.doc.return_date < frm.doc.transaction_date) {
+
+                frappe.throw("Return Date cannot be before the Transaction Date.");
+
+            }
+        }
+    }
+
+});
